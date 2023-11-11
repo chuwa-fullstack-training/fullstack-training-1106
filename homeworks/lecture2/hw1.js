a65609101b41204a6e2f30d631e71203455f1484
smudgebaby <zxy1307@bu.edu>
2023-11-11 04:46:44 -0500

    update lecture2 hw answers

--- homeworks/lecture2/hw1.js
+++ homeworks/lecture2/hw1.js
@@ -4,24 +4,68 @@
 * This function does not handle getters and setters or copy attributes.
 */
 function extend(o, p) {
-    // implement your code here
+    for(let prop in p){
+        o[prop] = p[prop];
+    }
+    return o;
 }
+
+const obj1 = { a: 1, b: 2 };
+const obj2 = { b: 3, c: 4 };
+const obj3 = {};
+const obj4 = { x: undefined, y: 10 };
+
+const result1 = extend(obj1, obj2);
+console.log(result1); // Output: { a: 1, b: 3, c: 4 }
+
+const result2 = extend(obj3, obj4);
+console.log(result2); // Output: { x: undefined, y: 10 }
+
 
 /*
 * Return a new object that holds the properties of both o and p.
 * If o and p have properties by the same name, the values from o are used.
 */
 function union(o, p) {
-    // implement your code here
+   let newObj = {};
+
+    for(const prop in o){
+        newObj[prop] = o[prop]
+    }
+    for(const prop in p){
+        if(!(prop in o)){
+            newObj[prop] = p[prop];
+        }
+    }
+    return newObj;
 }
+
+const result3 = union(obj1, obj2);
+console.log(result3) ; // Output: { a: 1, b: 2, c: 4 }
+
+const result4 = union(obj3, obj4);
+console.log(result4); // Output: { x: undefined, y: 10 }
+
 
 /*
 * Remove properties from o if there is not a property with the same name in p.
 * Return o.
 */
 function restrict(o, p) {
-    // implement your code here
+    for(let prop in o){
+        if(!(prop in p)){
+            delete(o[prop])
+        }
+    }
+    return o;
 }
+
+const result5 = restrict(obj1, obj2);
+console.log(result5) ; // Output: { b: 2 }
+
+const result6 = restrict(obj3, obj4);
+console.log(result6); // Output: {}
+
 
 /*
 * Return a new object that holds only the properties of o that also appear
@@ -29,5 +73,17 @@ function restrict(o, p) {
 * the properties in p are discarded
 */
 function intersection(o, p) {
-    // implement your code here
+    let newObj = {};
+    for(let prop in o){
+        if(prop in p){
+            newObj[prop] = o[prop]
+        }
+    }
+    return newObj;
 }
+
+const result7 = intersection(obj1, obj2);
+console.log(result7) ; // Output: { b: 2 }
+
+const result8 = intersection(obj3, obj4);
+console.log(result8); // Output: {}
--- homeworks/lecture2/hw3.js
+++ homeworks/lecture2/hw3.js
@@ -2,29 +2,108 @@
 // Explain your answer.
 
 console.log(0.1 + 0.2);
+/**
+ * Answer: 0.30000000000000004
+ * JavaScript uses IEEE-754 basic 64-bit binary floating-point for its Number type. 
+ * Using IEEE-754, the result of .1 + .2 is exactly 0.3000000000000000444089209850062616169452667236328125. 
+ * This results from:
+ * Converting “.1” to the nearest value representable in the Number type.
+ * Converting “.2” to the nearest value representable in the Number type.
+ * Adding the above two values and rounding the result to the nearest value representable in the Number type.
+ * 
+ * When formatting this Number value for display, 
+ * “0.30000000000000004” has just enough significant digits to uniquely distinguish the value. 
+ * To see this, observe that the neighboring values are:
+ * 0.299999999999999988897769753748434595763683319091796875,
+ * 0.3000000000000000444089209850062616169452667236328125, and
+ * 0.300000000000000099920072216264088638126850128173828125.
+ * If the conversion to a decimal numeral produced only “0.3000000000000000”, 
+ * it would be nearer to 0.299999999999999988897769753748434595763683319091796875 than 
+ * to 0.3000000000000000444089209850062616169452667236328125. 
+ * Therefore, another digit is needed. When we have that digit, “0.30000000000000004”, 
+ * then the result is closer to 0.3000000000000000444089209850062616169452667236328125 than to either of its neighbors.
+ */
 
 console.log(0.1 + 0.2 == 0.3);
+/**
+ * Answer: false
+ * 0.1 + 0.2 == 0.30000000000000004
+ */
 
 console.log(1 +  "2" + "2");
+/**
+ * Answer: 122
+ * "2" is string, Javascript convert 1 to string, so result will be 122.
+ */
 
 console.log(1 +  +"2" + "2");
+/**
+ * Answer: 32
+ * + operand in front of a string converts it to number. so +"2" will become number 2 and the result will be 3.
+ * and the second "2" is still string, so the final result will be 32.
+ */
 
 console.log(1 +  -"1" + "2");
+/**
+ * Answer: 02
+ * - operand in front of a string converts it to number. so -"1" will become number 1 and the result will be 0.
+ * and "2" is still string, so the final result will be 02.
+ * 
+ */
 
 console.log(+"1" +  "1" + "2");
+/**
+ * Answer: 112
+ * + operand in front of a string converts it to number. so +"1" will become number 1.
+ * and the second "1" is still string, will convert it to string concatenation
+ * so the final result will be 112.
+ */
 
 console.log( "A" - "B" + "2");
+/**
+ * Answer: NaN2
+ * "A" and "B" are both string, substract string will result NaN, and NaN append string, will be NaN2.
+ */
 
 console.log( "A" - "B" + 2);
+/**
+ * Answer: NaN
+ * "A" and "B" are both string, substract string will result NaN, and NaN add  number 2, will be NaN.
+ */
 
 console.log("0 || 1 = "+(0 || 1));
+/**
+ * Answer: 0 || 1 = 1
+ * "0 || 1 = " is string, and (0 || 1) is first truthy value which is 1, will concad to string, so result will be 0 || 1 = 1.
+ */
 
 console.log("1 || 2 = "+(1 || 2));
+/**
+ * Answer: 1 || 2 = 1
+ * "1 || 2 = " is string, and (1 || 2) is first truthy value which is 1, will concad to string, so result will be 1 || 2 = 1.
+ */
 
 console.log("0 && 1 = "+(0 && 1));
+/**
+ * Answer: 0 && 1 = 0
+ * "0 && 1 = " is string, and (0 && 1) is first falsey value which is 0, will concad to string, so result will be 0 && 1 = 0.
+ */
 
 console.log("1 && 2 = "+(1 && 2));
+/**
+ * Answer: 1 && 2 = 2
+ * "1 && 2 = " is string, and (1 && 2) returns the last value because 1 && 2 is truthy. 
+ * And will concad to string, so result will be 1 && 2 = 2.
+ */
 
 console.log(false == '0')
+/**
+ * Answer: true
+ * == checks whether two value is equal, and perform type coercion to convert 0 to false, so result is true.
+ */
 
 console.log(false === '0')
+/**
+ * Answer: false
+ * === checks both value and type, type of false is boolean but type of '0' is string, so result is false.
+ */
