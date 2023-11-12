3d77587658e45bc7e90d708bdb89e635aee4ef68
smudgebaby <74436502+smudgebaby@users.noreply.github.com>
2023-11-12 18:33:02 -0500

    update lecture2 hw answers

--- homeworks/lecture2/hw5.js
+++ homeworks/lecture2/hw5.js
@@ -4,15 +4,15 @@
 var x;
 
 if (x !== 3) {
-  console.log(y);
+  console.log(y);   // undefined
   var y = 5;
   if (y === 5) {
     var x = 3;
   }
-  console.log(y);
+  console.log(y);   // 5
 }
 if (x === 3) {
-  console.log(y);
+  console.log(y);   // 5
 }
 
 
@@ -20,7 +20,7 @@ if (x === 3) {
 var x = 3;
 if (x === 3) {
   var x = 2;
-  console.log(x);
+  console.log(x);   // 2
 }
-console.log(x);
+console.log(x);   // 2
 
--- homeworks/lecture2/hw6.js
+++ homeworks/lecture2/hw6.js
@@ -2,15 +2,76 @@
 
 // 1. Write a function that returns the largest element in a list.
 function largestElement(list) {
-    // implement your code here
+    let largest = list[0];
+    for(let i = 0; i<list.length; i++){
+        if(list[i] > largest){
+            largest = list[i];
+        }
+    }
+    return largest;
 }
+
+// Test Cases:
+const list1 = [1,2,3555,5,6,2,32, -9]
+console.log(largestElement(list1))      // Output: 3555
+
+const list2 = []
+console.log(largestElement(list2))      // Output: undefined
+
+const list3 = [-9]
+console.log(largestElement(list3))      // Output: -9
+
+
 
 // 2. Write function that reverses a list, preferably in place.
 function reverseList(list) {
-    // implement your code here
+    let i = 0;
+    let j = list.length-1;
+    while(i<j){
+        let temp = list[i];
+        list[i] = list[j];
+        list[j] = temp;
+        i+=1;
+        j-=1;
+    }
+    return list;
 }
+
+// Test Cases:
+const list4 = [1,2,3555,5,6,2,2, -9]
+console.log(reverseList(list4))      // Output: [-9, 2, 2, 6, 5, 3555, 2, 1]
+
+const list5 = [2,3555,5,6,2,32,-9]
+console.log(reverseList(list5))      // Output: [-9, 32, 2, 6,  5, 3555, 2]
+
+const list6 = []
+console.log(reverseList(list6))      // Output: []
+
+const list7 = [2]
+console.log(reverseList(list7))      // Output: [2]
+
+
 
 // 3. Write a function that checks whether an element occurs at least twice in a list.
 function checkTwice(list, element) {
-    // implement your code here
+    let occurance = 0;
+    for(let i = 0; i<list.length; i++){
+        if(list[i]===element){
+            occurance+=1;
+        }
+    }
+    return occurance>0;
 }
+
+// Test Cases:
+const list8 = [1,2,3555,5,6,2,2, -9]
+console.log(checkTwice(list8, 2))      // Output: true 
+
+const list9 = [999991,2,3555,5,6,2,32,-9]
+console.log(checkTwice(list9, 1))      // Output: false
+
+const list10 = []
+console.log(checkTwice(list10, 1))      // Output: false
+
+const list11 = [2,2,2,2,2,2]
+console.log(checkTwice(list11, 2))      // Output: true
