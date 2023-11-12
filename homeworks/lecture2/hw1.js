d4370d60de43f7e3763b8db06a4df5dfb8fc2b1a
rui221 <72562873+rui221@users.noreply.github.com>
2023-11-11 18:03:48 -0800

    hw3

--- homeworks/lecture2/hw1.js
+++ homeworks/lecture2/hw1.js
@@ -5,7 +5,7 @@
 */
 function extend(o, p) {
     // implement your code here
-    // implement your code here
+    
 }
 
 /*
--- homeworks/lecture2/hw3.js
+++ homeworks/lecture2/hw3.js
@@ -2,29 +2,30 @@
 // Explain your answer.
 
 console.log(0.1 + 0.2);
-
+//0.30000000000000004 (float poin add)
 console.log(0.1 + 0.2 == 0.3);
-
+//False 0.30000000000000004!=0.3
 console.log(1 +  "2" + "2");
-
+//122 beacuse 1 +  "2" change to string 12 then "12" + "2" become "122" 
 console.log(1 +  +"2" + "2");
-
+//32 beacuse 1++"2" = 1 + 2, then 3 + "2" = "32"
 console.log(1 +  -"1" + "2");
-
+//02 because 1+ -"1" = 0 then 0 + "2" = "02"
 console.log(+"1" +  "1" + "2");
-
+//112 beceuse (+"1" +  "1" + "2") = (1 +  "2" + "2")
 console.log( "A" - "B" + "2");
-
+//"NaN2" because "A" - "B" not a number and then add "2"
 console.log( "A" - "B" + 2);
-
+//NaN  when you add a number to NAN is always = NAN
 console.log("0 || 1 = "+(0 || 1));
-
+//"0 || 1 = 1"
 console.log("1 || 2 = "+(1 || 2));
-
+//"1 || 2 = 1"
 console.log("0 && 1 = "+(0 && 1));
-
+//"0 && 1 =0"
 console.log("1 && 2 = "+(1 && 2));
-
+//"1 && 2 = 1"
 console.log(false == '0')
-
+//false
 console.log(false === '0')
+//false
