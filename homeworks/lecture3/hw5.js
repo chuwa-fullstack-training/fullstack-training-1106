2223cda813bc90a5e590b251a00c59c1e6479b37
Xiaofan Lin <xiaofanlinus@gmail.com>
2023-11-14 15:48:43 -0800

    finish l3hw5

--- homeworks/lecture3/hw5.js
+++ homeworks/lecture3/hw5.js
@@ -13,4 +13,31 @@
  */
 function User() {
     // implement here
+    let password;
+
+    this.setPassword = function (p) {
+        if (password !== undefined) {
+            throw Error("Password has been defined!")
+        } else {
+            password = p;
+        }
+    }
+
+    this.checkPassword = function (p) {
+        return password === p;
+    }
 }
+
+
+const user = new User();
+user.setPassword('123456');
+console.log(user.checkPassword('123456')); // true
+console.log(user.checkPassword('123')); // false
+console.log(user.password); // undefined
+try {
+    user.setPassword('123'); // Error
+} catch (e) {
+    console.log(e.message);
+}
+console.log(user.checkPassword('123')); // false
+console.log(user.password); // undefined
