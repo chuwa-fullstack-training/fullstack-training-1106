9bc0694648eab8e91d955db0091bad72746cbc91
smudgebaby <74436502+smudgebaby@users.noreply.github.com>
2023-11-14 15:42:11 -0500

    upload lecture3 hw5

--- homeworks/lecture3/hw5.js
+++ homeworks/lecture3/hw5.js
@@ -12,5 +12,36 @@
  * user.password; // undefined
  */
 function User() {
-    // implement here
+    var password = '';
+
+    function setPassword(str){
+        if(str.length >= 4){
+            password = str;
+        } else{
+            throw new Error('Error!')
+        }
+    }
+
+    function checkPassword(str){
+        return password === str;
+    }
+
+    return {
+        setPassword: setPassword,
+        checkPassword: checkPassword,
+    }
 }
+
+
+const user = new User();
+user.setPassword('123456');
+console.log(user.checkPassword('123456')); // true
+console.log(user.checkPassword('123')); // false
+console.log(user.password); // undefined
+try {
+    user.setPassword('123');
+} catch (error) {
+    console.error(error.message); // Error
+}
+console.log(user.checkPassword('123')); // false
+console.log(user.password); // undefined
