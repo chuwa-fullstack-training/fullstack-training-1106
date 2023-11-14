7bf7872b6b50a84f024ed7cb90443a14c51197e6
Xiaofan Lin <xiaofanlinus@gmail.com>
2023-11-13 16:22:12 -0800

    create new branch and do some hw

--- homeworks/lecture2/hw1.js
+++ homeworks/lecture2/hw1.js
@@ -1,33 +1,53 @@
 /*
-* Copy the enumerable properties of p to o, and return o.
-* If o and p have a property by the same name, o's property is overwritten.
-* This function does not handle getters and setters or copy attributes.
-*/
+ * Copy the enumerable properties of p to o, and return o.
+ * If o and p have a property by the same name, o's property is overwritten.
+ * This function does not handle getters and setters or copy attributes.
+ */
 function extend(o, p) {
-    // implement your code here
+  // implement your code here
+  for (let key in p) {
+    o[key] = p[key];
+  }
+  o.__proto__ = p.__proto__;
+  return o;
 }
 
 /*
-* Return a new object that holds the properties of both o and p.
-* If o and p have properties by the same name, the values from o are used.
-*/
+ * Return a new object that holds the properties of both o and p.
+ * If o and p have properties by the same name, the values from o are used.
+ */
 function union(o, p) {
-    // implement your code here
+  // implement your code here
 }
 
 /*
-* Remove properties from o if there is not a property with the same name in p.
-* Return o.
-*/
+ * Remove properties from o if there is not a property with the same name in p.
+ * Return o.
+ */
 function restrict(o, p) {
-    // implement your code here
+  // implement your code here
 }
 
 /*
-* Return a new object that holds only the properties of o that also appear
-* in p. This is something like the intersection of o and p, but the values of
-* the properties in p are discarded
-*/
+ * Return a new object that holds only the properties of o that also appear
+ * in p. This is something like the intersection of o and p, but the values of
+ * the properties in p are discarded
+ */
 function intersection(o, p) {
-    // implement your code here
+  // implement your code here
 }
+
+// extend test case:
+let o = {
+  name: "hello",
+  age: 5,
+};
+
+let p = {
+  age: 25,
+  major: "computer science",
+  gpa: 3.5,
+};
+
+console.log(extend(o, p));
+console.log(o);
