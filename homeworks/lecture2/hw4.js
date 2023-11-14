82313cdae8b8e6d93492a393da34149f2cd7099f
Xiaofan Lin <xiaofanlinus@gmail.com>
2023-11-13 20:55:11 -0800

    finish l2hw4

--- homeworks/lecture2/hw4.js
+++ homeworks/lecture2/hw4.js
@@ -8,6 +8,12 @@ function f() {
   }
   console.log(a);
 }
+/**
+ * Output: 7
+ * a will be in scope and its value will be 7 because
+ * we enter if statment when a > 5 is true because a was
+ * 10 before. During if statement, a is modified to 7
+ */
 
 // 2. When executed, what value will be output?
 function f() {
@@ -16,6 +22,15 @@ function f() {
   }
   console.log(a);
 }
+/**
+ * Output: 5
+ * "a" is still in scope and accessible
+ * because it uses var to declare "a"
+ * even it is defined within if statement. Becase a is
+ * within the same function scope, we can still access it.
+ * a is 5 when it got output to the console.
+ *
+ */
 
 // 3. When executed, what value will be output?
 function f() {
@@ -23,6 +38,16 @@ function f() {
 }
 f();
 console.log(a);
+/**
+ * Output: 3
+ *
+ * Since a is not defined in either block-scope, function-scope or global-scope.
+ * JS will look for global object in the environment which will be window in
+ * client-side browser. And assign window.a to 3 as if we use "var a = 3" in the
+ * global scope. When a is logged afterward, a will be the result of window.a
+ * which in this case is 3 output to the console.
+ *
+ */
 
 // 4.
 var a = 5;
@@ -35,6 +60,14 @@ function second() {
 }
 first();
 second();
+/**
+ * Output: 6
+ * 'var a = 5;' defines a global scope variable 'a' with 5
+ * In "first()", a in global scope is modified to 6.
+ * In "second()", a in global scope is accessed and output
+ * to the console with value 6.
+ *
+ */
 
 // 5.
 var a = 5;
@@ -42,6 +75,13 @@ function f() {
   var a = 7;
   console.log(a);
 }
+/**
+ * Output: 7
+ *
+ * "var a = 7" in f() masks out the global scope variable "a"
+ * outside of f(). It is then accessed with value 7 and output
+ * to the console.
+ */
 
 // 6.
 var a = 1;
@@ -52,3 +92,15 @@ function b() {
 }
 b();
 console.log(a);
+/**
+ * Output: 1
+ * Within b(), function a() {} declaration
+ * is moved to the top of b(). As a result,
+ * it masks out the global scope variable 'a'
+ * outside of b(). Variable 'a' in b() is then
+ * modified to 10 but it will not affect the value
+ * of global 'a' outside b() because of variable
+ * masking. When we are out of b() and
+ * console.log(a), the output is 1.
+ *
+ */
--- homeworks/lecture2/hw5.js
+++ homeworks/lecture2/hw5.js
@@ -14,7 +14,29 @@ if (x !== 3) {
 if (x === 3) {
   console.log(y);
 }
-
+/**
+ * output:
+ * undefined
+ * 5
+ * 5
+ *
+ * why?
+ * We declare x with undefined value.
+ * The if statment evaluates to true because undefined is not 3
+ * entering the if statment afterward.
+ * We declare y using var which effectively move the declarion
+ * on top of current scope which is global scope. But we don't
+ * move the definition. As a result, the first output is 'undefined'.
+ * When we move to 'var y = 5', we simply change y to 5. We then
+ * enter the if statment sine it evaluates to true,
+ * execute "var x = 3". Because we are not in
+ * function, x will be a global variable with value 3.
+ * We then log for the second time. But for this time, y is 5 so it got
+ * printed to the console. Then we enter the second if statment because
+ * x is 3, we then print y for the third time with a value of 5 again.
+ *
+ *
+ */
 
 // 2.
 var x = 3;
@@ -23,4 +45,16 @@ if (x === 3) {
   console.log(x);
 }
 console.log(x);
-
+/**
+ * output:
+ * 2
+ * 2
+ *
+ * Why?
+ * x is set to 3 in the global scope. We then enter the if statement,
+ * x is redeclared again with 'var'. But since we are not in function
+ * scope, variable masking is not in effect. 'var x' declaration will be moved
+ * to the top of global scope. Global variable x is redefined to 2.
+ * When we print 'a' within the if statment, the output will be 2.
+ * When we print 'a' for the second time, the output will still be 2.
+ */
