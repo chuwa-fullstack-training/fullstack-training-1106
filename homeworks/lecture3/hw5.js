e6a03134a62698ec16a53247a63a39de7ef4277d
Guangwei Na <gna@stevens.edu>
2023-11-15 12:23:17 -0500

    lec3-hw5

--- homeworks/lecture3/hw5.js
+++ homeworks/lecture3/hw5.js
@@ -13,4 +13,22 @@
  */
 function User() {
     // implement here
+    var password = "";
+    function setPassword(str) {
+        password = str;
+    }
+    function checkPassword(str){
+        if(str === password) return true;
+        else return false;
+    }
+    return {
+        setPassword: setPassword,
+        checkPassword: checkPassword
+    }
 }
+
+const user = new User();
+console.log(user.password);
+user.setPassword("iuiu");
+console.log(user.checkPassword("iuiu"));
+console.log(user.checkPassword("opop"));
