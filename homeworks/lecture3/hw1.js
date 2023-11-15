7075ed211a10f6368a083a49bb88fb46b37e371a
Guangwei Na <gna@stevens.edu>
2023-11-14 21:29:29 -0500

    lec3-hw1

--- homeworks/lecture3/hw1.js
+++ homeworks/lecture3/hw1.js
@@ -3,6 +3,33 @@
  * pick 48 coins to have 1 dollar
  * print out 2 solutions
  */
+var ans = []
 function pickCoins() {
     // implement here
+    combination(0,100,[]);
+    return ans;
 }
+
+var combination = function get(num, value, res){
+    if(ans.length==2) return;
+    if(value<0 || num > 48) return;
+    if(value === 0){
+        if(num === 48) ans.push(res.slice());
+        else return;
+    }else{
+        res.push(1);
+        get(num+1,value-1,res);
+        res.pop();
+        res.push(5);
+        get(num+1,value-5,res);
+        res.pop();
+        res.push(25);
+        get(num+1,value-25,res);
+        res.pop();
+        res.push(50);
+        get(num+1,value-50,res);
+        res.pop();
+    }
+}
+
+pickCoins()
