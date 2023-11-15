d3241232cf4da410c5b097ceaf6294bb38c151ad
Guangwei Na <gna@stevens.edu>
2023-11-14 21:43:06 -0500

    lec3-hw2

--- homeworks/lecture3/hw2.js
+++ homeworks/lecture3/hw2.js
@@ -4,4 +4,10 @@
  */
 function sum() {
     // implement here
+    if (arguments.length == 2) return arguments[0] + arguments[1];
+    var a = arguments[0];
+    var subSum = function(b){ return a+b;}
+    return subSum;
 }
+console.log(sum(2,3));
+console.log(sum(2)(3));
