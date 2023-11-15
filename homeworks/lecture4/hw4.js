51b520e2ad657639ff799756ec2f40544501b6a7
Guangwei Na <gna@stevens.edu>
2023-11-15 15:23:32 -0500

    lec4-hw4

--- homeworks/lecture4/hw4.js
+++ homeworks/lecture4/hw4.js
@@ -1,5 +1,6 @@
 /**
- * Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must be unique and you may return the result in any order.
+ * Given two integer arrays nums1 and nums2, return an array of their intersection. 
+ * Each element in the result must be unique and you may return the result in any order.
  *
  * Example 1:
  * Input: nums1 = [1,2,2,1], nums2 = [2,2]
@@ -12,4 +13,14 @@
  */
 const intersection = (nums1, nums2) => {
   // Your solution here
+  var set = new Set();
+  var ans = new Set();
+  for(let i of nums1){
+    set.add(i);
+  }
+  for(let i of nums2){
+    if(set.has(i)) ans.add(i);
+  }
+  return [...ans];
 };
+console.log(intersection([4,5,9],[9,4,9,8,4]));
