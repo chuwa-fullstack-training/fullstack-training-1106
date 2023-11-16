5c89eeb668a6c1e78399ec767bb45cc83f2ee21c
smudgebaby <74436502+smudgebaby@users.noreply.github.com>
2023-11-16 17:25:40 -0500

    upload lecture4 hw answers

--- homeworks/lecture4/hw3.js
+++ homeworks/lecture4/hw3.js
@@ -8,3 +8,38 @@
  */
 
 // your code here
+
+// ES5
+var SingletonConstructorES5 = (function() {
+    var instance;
+    function Singleton() {
+        if(instance) {
+            return instance;
+        }
+        instance = this;
+    }
+    Singleton.getInstance = function() {
+        return instance || new Singleton();
+    }
+    return Singleton;
+}());
+
+const instance1ES5 = new SingletonConstructorES5();
+const instance2ES5 = new SingletonConstructorES5();
+console.log(instance1ES5 === instance2ES5); // Output: true
+
+
+
+// ES6
+class SingletonES6 {
+    constructor() {
+        if(SingletonES6.instance) {
+            return SingletonES6.instance
+        }
+        SingletonES6.instance = this;
+    }
+}
+
+const instance1ES6 = new SingletonES6();
+const instance2ES6 = new SingletonES6();
+console.log(instance1ES6 === instance2ES6); // Output: true
--- homeworks/lecture4/hw4.js
+++ homeworks/lecture4/hw4.js
@@ -1,5 +1,6 @@
 /**
- * Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must be unique and you may return the result in any order.
+ * Given two integer arrays nums1 and nums2, return an array of their intersection. 
+ * Each element in the result must be unique and you may return the result in any order.
  *
  * Example 1:
  * Input: nums1 = [1,2,2,1], nums2 = [2,2]
@@ -11,5 +12,21 @@
  *
  */
 const intersection = (nums1, nums2) => {
-  // Your solution here
+  let result = new Set();
+  for(element of nums1){
+      if(nums2.includes(element)){
+          result.add(element)
+      }
+  }
+  return [...result]
 };
+
+// test cases
+const nums1 = [1,2,2,1]
+const nums2 = [2,2]
+
+console.log(intersection(nums1, nums2)) // Output: [2]
+
+const nums3 = [4,9,5]
+const nums4 = [9,4,9,8,4]
+console.log(intersection(nums3, nums4)) //Output: [9,4]
