b1f92ac1e26401e45ffd3b1f72b0200a678b0df6
smudgebaby <74436502+smudgebaby@users.noreply.github.com>
2023-11-17 16:20:36 -0500

    upload lecture4 hw5 answer

--- homeworks/lecture4/hw5.js
+++ homeworks/lecture4/hw5.js
@@ -6,6 +6,27 @@
 // }
 // data.child = data;
 
-const cloneDeepWithLoop = (obj) => {
-    // Implement the function here
+const cloneDeepWithLoop = (obj, clones = new Set()) => {
+    if(obj === null || typeof obj !== 'object') {
+        return obj;
+    }
+    if(clones.has(obj)){
+        return obj;
+    }
+    clones.add(obj);
+    const newCopy = {};
+    for(const key in obj) {
+        newCopy[key] = cloneDeepWithLoop(obj[key], clones);
+    }
+    return newCopy;
 }
+
+
+const data = {
+    name: 'foo',
+    child: null
+};
+data.child = data;
+
+const newData = cloneDeepWithLoop(data);
+console.log(newData); // Output: { name: 'foo', child: [Circular] }
