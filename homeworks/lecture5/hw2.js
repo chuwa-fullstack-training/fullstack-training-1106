1f93567ace8c3774bfccca9d3071905754e94570
Xiaofan Lin <xiaofanlinus@gmail.com>
2023-11-17 15:18:41 -0800

    finish l5hw2

--- homeworks/lecture5/hw2.js
+++ homeworks/lecture5/hw2.js
@@ -9,7 +9,27 @@
  */
 function reverseWords(str) {
   // your code here
+  let curr = 0;
+  let left = 0;
+  let right = 0;
+  str.reverse();
+  while (curr < str.length) {
+    left = curr;
+    right = curr;
+    while (right < str.length && str[right] != ' ') {
+      right++;
+    }
+    curr = right + 1;
+    right--;
+    while (left <= right) {
+      [str[left], str[right]] = [str[right], str[left]];
+      left++
+      right--;
+    }
+  }
+  return str;
 }
 
 const input = 'the sky is blue'.split(''); // ['t', 'h', 'e', ' ', 's', 'k', 'y', ' ', 'i', 's', ' ', 'b', 'l', 'u', 'e']
 reverseWords(input);
+console.log(input);
