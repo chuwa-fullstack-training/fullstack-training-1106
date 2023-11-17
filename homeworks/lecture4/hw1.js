dbb50c7997f88c549dd26f4b016d99721eb89880
Xiaofan Lin <xiaofanlinus@gmail.com>
2023-11-16 20:11:50 -0800

    finish l4hw1

--- homeworks/lecture4/hw1.js
+++ homeworks/lecture4/hw1.js
@@ -7,4 +7,39 @@
 
 function checkValidHTML(html) {
     // implement your solution here
+    let str = html;
+    let rex = /(<\w+>)|(<\/\w+>)/;
+    let lp = /(<\w+>)/;
+    //let rp = /(<\/\w+>)/;
+    let tagRe = /\w+/
+    let cleared = str.split(rex).filter(v => rex.test(v));
+    let stack = [];
+    for (let tag of cleared) {
+        if (lp.test(tag)) {
+            stack.push(tag);
+        } else {
+            if (stack.length) {
+                let leftTag = stack.pop();
+                let leftTagName = leftTag.match(tagRe)[0];
+                let rightTagName = tag.match(tagRe)[0];
+                if (leftTagName != rightTagName) {
+                    return false;
+                }
+            } else {
+                return false;
+            }
+
+        }
+    }
+    if (stack.length) {
+        return false;
+    }
+    return true;
 }
+
+let str = "<html><head><title>My Title</title></head></html>";
+console.log(checkValidHTML(str));
+str = "<html><head><title>My Title</title></head></head></html>";
+console.log(checkValidHTML(str));
+str = "<html><head><title>My Title</title></head></html";
+console.log(checkValidHTML(str));
---
+++ homeworks/lecture4/test.js
@@ -0,0 +1,5 @@
+const myString = "Hello 1 word. Sentence number 2.";
+const splits = myString.split(/\d/);
+
+console.log(splits);
+// [ "Hello ", "1", " word. Sentence number ", "2", "." ]
