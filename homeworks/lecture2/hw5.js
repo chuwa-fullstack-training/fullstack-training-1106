f2b6b9c65f40cb72fecce5c8eec9d092862fc40c
Guangwei Na <gna@stevens.edu>
2023-11-17 13:18:52 -0500

    lec2-hw5

--- homeworks/lecture2/hw5.js
+++ homeworks/lecture2/hw5.js
@@ -14,13 +14,19 @@ if (x !== 3) {
 if (x === 3) {
   console.log(y);
 }
+// First, x has no value initialized so x is undefined and we will get into the first if block,
+// when console y, there won't be an error because var y will be hoisted to function scope.
+// Second, y will be assigned 5, then goes into the inner if block and assign x as 3.
+//Finally console y = 5, and get into the last if block and console y as 5 again.
 
 
 // 2.
-var x = 3;
-if (x === 3) {
-  var x = 2;
-  console.log(x);
-}
-console.log(x);
+// var x = 3;
+// if (x === 3) {
+//   var x = 2;
+//   console.log(x);
+// }
+// console.log(x);
 
+// when the statement in if block is is executed, variable x will be overwritten to 2, and as var is effective
+//of the scope of function, the second console will still print out x as 2;
