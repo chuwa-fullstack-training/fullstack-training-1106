959a183223ab732c3584b09962c7776edac5d2c2
Guangwei Na <gna@stevens.edu>
2023-11-20 16:34:10 -0500

    lec5-hw2

--- homeworks/lecture5/hw2.js
+++ homeworks/lecture5/hw2.js
@@ -9,7 +9,14 @@
  */
 function reverseWords(str) {
   // your code here
+  const input = str.split(' ');
+  var ans = [];
+  for(let i=input.length-1;i>=0;i--){
+    ans.push(input[i]);
+  } 
+  return ans.join(' ');
 }
 
-const input = 'the sky is blue'.split(''); // ['t', 'h', 'e', ' ', 's', 'k', 'y', ' ', 'i', 's', ' ', 'b', 'l', 'u', 'e']
-reverseWords(input);
+// const input = 'the sky is blue'.split(''); // ['t', 'h', 'e', ' ', 's', 'k', 'y', ' ', 'i', 's', ' ', 'b', 'l', 'u', 'e']
+str = "the sky is blue"
+console.log(reverseWords(str));
