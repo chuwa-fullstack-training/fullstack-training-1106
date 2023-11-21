5e9d9120585bcffe455f8b7a0d2c5ad0aa4ef649
Guangwei Na <gna@stevens.edu>
2023-11-20 19:10:57 -0500

    lec5-hw6

--- homeworks/lecture5/hw6.js
+++ homeworks/lecture5/hw6.js
@@ -4,21 +4,67 @@
  * @param {string[]} urls - an array of urls
  * @returns {any[]} - an array of responses
  */
-function sequencePromise(urls) {
+
+import { get } from 'https';
+
+async function sequencePromise(urls) {
   const results = [];
   function fetchOne(url) {
     // for `getJSON` function you can choose either from the implementation of hw5 or `fetch` version provided by browser
     // if you use `fetch`, you have to use browser console to test this homework
-    return getJSON(url).then(response => results.push(response));
+    return getJSON(url).then(response => 
+      results.push(response));
   }
   // implement your code here
-
+  var req = [];
+  for(let u of urls){
+     req.push(new fetchOne(u));
+  }
+  let tmp = await Promise.all(req);
+  for(let i of tmp){
+    results.push(tmp);
+  }
   return results;
+  
 }
 
 // option 1
 function getJSON(url) {
   // this is from hw5
+  const options = {
+    headers: {
+      'User-Agent': 'request'
+    }
+  };
+return new Promise((resolve,reject) => {
+const request = get(url, options, response => {
+    if (response.statusCode !== 200) {
+      reject(
+        `Did not get an OK from the server. Code: ${response.statusCode}`
+      );
+      response.resume();
+    }
+
+    let data = '';
+    response.on('data', chunk => {
+      data += chunk;
+    });
+    response.on('end', () => {
+      try {
+        // When the response body is complete, we can parse it and log it to the console
+        resolve(JSON.parse(data));
+      } catch (e) {
+        // If there is an error parsing JSON, log it to the console and throw the error
+        reject(new Error(e.message));
+      }
+    });
+  });
+  request.on('error', err => {
+    reject(
+      `Encountered an error trying to make a request: ${err.message}`
+    );
+  });
+})
 }
 
 // option 2
@@ -32,3 +78,5 @@ const urls = [
   'https://api.github.com/search/repositories?q=react',
   'https://api.github.com/search/repositories?q=nodejs'
 ];
+var res = await sequencePromise(urls)
+console.log(res);
