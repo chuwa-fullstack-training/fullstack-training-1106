3d61101b3de990b427b91b952e69e27ff6b575a7
Boxiao Yan <boy033@ucsd.edu>
2023-11-21 23:01:46 -0800

    hw for lec6

--- homeworks/lecture6/hw3.js
+++ homeworks/lecture6/hw3.js
@@ -1,11 +1,27 @@
 /**
  * implement debounce function
  * explain: `func` will be called after `delay` ms. if `func` is called again before `delay` ms, the timer will be reset
+ * implement a decorator to delay the calling of the input function `func` by `setTimeout`.
+ * When `debounce` is called again with the `func`. Delete the last `setTimeout` call and set a new one. 
+ * 
+ * usage:
+ * button.addEventListener("click", onClickCallback);
+ * button.addEventListener("click", debounce(onClickCallback, 300));
+ * 
  * @param func
  * @param delay
  */
 function debounce(func, delay) {
   // your code here
+  let timer;
+  return (...args) => {
+    // clear the last setTimeout
+    clearTimeout(timer);
+    // create a new setTimeout
+    timer = setTimeout(() => {
+      func.apply(this, args);
+    }, delay);
+  }
 }
 
 /**
@@ -14,4 +30,13 @@ function debounce(func, delay) {
  */
 function throttle(func, delay) {
   // your code here
+  let lastTime = 0;
+  return (...args) => {
+    let currTime = new Date().getTime(); 
+    // if delay time is passed, call the function
+    if (currTime - lastTime > delay) {
+      func.apply(this, args);
+      lastTime = currTime;
+    }
+  }
 }
--- homeworks/lecture6/hw5.js
+++ homeworks/lecture6/hw5.js
@@ -1,7 +1,19 @@
 // 1. use `promise` to print 1, 2, 3 in every 1 second
-function print() {
+function print(val) {
   // your code here
+  return new Promise(resolve =>
+    setTimeout(() => {
+      console.log(val);
+      resolve();
+    }, 1000)
+  );
 }
+
+(async function () {
+  for (let i = 1; i <= 3; i++) {
+    await print(i);
+  }
+})();
 
 // improved: print every single numbers in a list in every 1 second
 // hint: `reduce`
@@ -9,11 +21,27 @@ const nums = [3, 1, 6, 9, 2];
 
 function printList() {
   // your code here
+  nums.reduce(async (promise, val) => {
+    return await promise.then(() => {
+      return print(val);
+    })
+  }, Promise.resolve());
 }
+
+printList();
 
 // 2. traffic light
 // output: red -> green -> yellow -> red -> ...
 // the delay time is up to you, but the order has to be correct
-function trafficLight() {
+async function trafficLight() {
   // your code here
+  const light = ["red", "green", "yellow"]
+  let count = 0;
+  while (true) {
+    count = count % light.length;
+    await print(light[count]);
+    count += 1;
+  }
 }
+
+trafficLight();
