9aa1bccddef718f08f5cf59e67de96fe4029949d
Full Stack Training <react.training@chuwaamerica.com>
2023-11-23 13:42:48 -0800

    update lecture 6 with hw

--- homeworks/lecture6/hw3.js
+++ homeworks/lecture6/hw3.js
@@ -1,8 +1,17 @@
 /**
  * implement debounce function
  * explain: `func` will be called after `delay` ms. if `func` is called again before `delay` ms, the timer will be reset
- * @param func
- * @param delay
+ * @example
+ * // after 1s, print 'hello'
+ * // However, if `printHello` is called again before 1s, the timer will be reset
+ * const printHello = () => console.log('hello')
+ * const debouncedFn = debounce(printHello, 1000)
+ * debouncedFn()
+ * debouncedFn() // timer reset to 1s
+ * 
+ * @param {function} func
+ * @param {number} delay
+ * @returns {function}
  */
 function debounce(func, delay) {
   // your code here
@@ -11,6 +20,17 @@ function debounce(func, delay) {
 /**
  * implement throttle function
  * explain: `func` will be called every `delay` ms. if `func` is called again before `delay` ms, the call will be ignored
+ * @example
+ * // after 1s, print 'hello'
+ * // However, if `printHello` is called again before 1s, the call will be ignored
+ * const printHello = () => console.log('hello')
+ * const throttledFn = throttle(printHello, 1000)
+ * throttledFn()
+ * throttledFn() // ignored
+ * 
+ * @param {function} func
+ * @param {number} delay
+ * @returns {function}
  */
 function throttle(func, delay) {
   // your code here
