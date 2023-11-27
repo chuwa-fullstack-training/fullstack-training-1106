b86e23d0e78e1d1e36513c84ac832960d20dd3c6
Kyrios0 <kyr1os@qq.com>
2023-11-27 15:49:07 -0800

    Misc Update
    add some comma

--- homeworks/lecture2/test.js
+++ homeworks/lecture2/test.js
@@ -1,6 +1,7 @@
 // testing script for lecture2 hw1 and hw6
-// hw1
 const assert = require('assert');
+
+// hw1
 const {extend, union, restrict, intersection} = require("./hw1.js")
 
 let o = {}, p = {};
@@ -24,7 +25,7 @@ assert.deepEqual(union(o, p), {a: 1, b: 2, c: 3, d: 6}, "[!] union check failed"
 assert.deepEqual(restrict(o, p), {b: 2, c: 3}, "[!] restrict check failed");
 [o, p] = reset();
 assert.deepEqual(intersection(o, p), {b: 2, c: 3}, "[!] intersection check failed");
-console.log("[+] All hw1 cases passed.")
+console.log("[+] All hw1 cases passed.");
 
 // hw6
 const {largestElement, reverseList, checkTwice} = require("./hw6.js")
@@ -32,4 +33,4 @@ assert.deepEqual(largestElement([5, 4, 6, 3, 8, 9]), 9, "[!] largestElement chec
 assert.deepEqual(reverseList([5, 4, 6, 3, 8, 9]), [9, 8, 3, 6, 4, 5], "[!] reverseList check failed");
 assert.deepEqual(checkTwice([5, 4, 6, 3, 8, 9, 8], 9), false, "[!] checkTwice check failed");
 assert.deepEqual(checkTwice([5, 4, 6, 3, 8, 9, 8], 8), true, "[!] checkTwice check failed");
-console.log("[+] All hw6 cases passed.")
+console.log("[+] All hw6 cases passed.");
--- homeworks/lecture3/test.js
+++ homeworks/lecture3/test.js
@@ -11,13 +11,13 @@ let checkCoins = solutions => {
     }
 }
 checkCoins(pickCoins());
-console.log("[+] All hw1 cases passed.")
+console.log("[+] All hw1 cases passed.");
 
 // hw2
 const sum = require("./hw2.js");
 assert.deepEqual(sum(2)(3), 5, "[!] sum check failed.");
 assert.deepEqual(sum(2, 3), 5, "[!] sum check failed.");
-console.log("[+] All hw2 cases passed.")
+console.log("[+] All hw2 cases passed.");
 
 // hw4
 const {Triangle, Circle, ES6Triangle, ES6Circle} = require("./hw4.js")
@@ -33,7 +33,7 @@ assert.deepEqual(es6Tri.getPerimeter(), 12, "[!] getPerimeter failed.");
 assert.deepEqual(es6Tri.getArea(), 6, "[!] getPerimeter failed.");
 assert.deepEqual(es6Cir.area(), 4*Math.PI, "[!] getPerimeter failed.");
 assert.deepEqual(es6Cir.circumference(), 4*Math.PI, "[!] getPerimeter failed.");
-console.log("[+] All hw4 cases passed.")
+console.log("[+] All hw4 cases passed.");
 
 // hw5
 const User = require("./hw5.js");
@@ -45,7 +45,7 @@ assert.deepEqual(user.password, undefined, "[!] password failed"); // undefined
 assert.deepEqual(user.setPassword('123'), false, "[!] setPasswd failed"); // Error
 assert.deepEqual(user.checkPassword('123'), false, "[!] checkPassword failed"); // false
 assert.deepEqual(user.password, undefined, "[!] password failed"); // undefined
-console.log("[+] All hw5 cases passed.")
+console.log("[+] All hw5 cases passed.");
 
 // hw6
 const {numIdenticalPairs, removeVowels} = require("./hw6.js")
@@ -53,4 +53,4 @@ assert.deepEqual(numIdenticalPairs([1,2,3,1,1,3]), 4, "[!] numIdenticalPairs che
 assert.deepEqual(numIdenticalPairs([1,1,1,1]), 6, "[!] numIdenticalPairs check failed");
 assert.deepEqual(numIdenticalPairs([1,2,3]), 0, "[!] numIdenticalPairs check failed");
 assert.deepEqual(removeVowels("asfuijhjafn"), "sfjhjfn", "[!] removeVowels check failed");
-console.log("[+] All hw6 cases passed.")
+console.log("[+] All hw6 cases passed.");
