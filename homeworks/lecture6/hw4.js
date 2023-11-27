5c7e684a072559f4e89512378d1ce3d011ea9806
Guangwei Na <gna@stevens.edu>
2023-11-27 01:57:30 -0500

    lec6-hw4

--- homeworks/lecture6/hw4.js
+++ homeworks/lecture6/hw4.js
@@ -6,4 +6,37 @@
  */
 function format(num) {
   // your code here
+  var ans = []
+  let flag = 0;
+  let str = num.toString();
+  if(Number.isInteger(num)){
+    for(let i=str.length-1;i>=0;i--){
+      if(flag === 3){
+        ans.unshift(',');
+        flag = 0;
+      }
+      ans.unshift(str[i]);
+      flag++;
+    }
+  }else{
+    let dot;
+    for(let i=str.length-1;i>=0;i--){
+      ans.unshift(str[i]);
+      if(str[i] === '.'){
+        dot = i;
+        break;
+      } 
+    }
+    for(let i= dot - 1;i>=0;i--){
+      if(flag === 3){
+        ans.unshift(',');
+        flag = 0;
+      }
+      ans.unshift(str[i]);
+      flag++;
+    }
+  }
+  return ans.join('');
 }
+
+console.log(format(12345678.1235655));
