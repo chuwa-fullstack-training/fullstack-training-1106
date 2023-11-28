eb353991d9a945a137aa49377c7272f3f4f32091
smudgebaby <74436502+smudgebaby@users.noreply.github.com>
2023-11-28 16:05:02 -0500

    upload lecture6 hw4 answer

--- homeworks/lecture6/hw4.js
+++ homeworks/lecture6/hw4.js
@@ -5,5 +5,23 @@
  * @param {number} num
  */
 function format(num) {
-  // your code here
+  num = num.toString();
+  let i = num.length-1;
+  if(num.indexOf('.') > 0){
+    i = num.indexOf('.') - 1;
+  }
+  while(i > 0) {
+    i -= 3;
+    if(i>=0){
+      num = num.slice(0,i+1) + ',' + num.slice(i+1, num.length)
+    }
+  }
+  return num;
 }
+
+
+console.log(format(123456.78)) // output: 123,456.78
+console.log(format(0)); // Output: 0
+console.log(format(1000000)); // Output: 1,000,000
+console.log(format(0.123456)); // Output: 0.123456
+console.log(format(1234567.899)); // Output: 1,234,567.899
