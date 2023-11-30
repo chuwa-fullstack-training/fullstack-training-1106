06ce11b2f83e22fc012dc79f7774e2b978df2c80
hangwei5139 <hangwei5139@gmail.com>
2023-11-30 15:27:58 -0800

    Added Homework Answers for Lecture 7

--- homeworks/lecture7/hw2.js
+++ homeworks/lecture7/hw2.js
@@ -19,3 +19,54 @@
  */
 
 // your code here
+
+const { error } = require('console');
+const http = require('http');
+const url = require('url');
+
+const server = http.createServer((req, res) => {
+    const parseUrl = url.parse(req.url, true);
+    const path = parseUrl.pathname;
+    const iso = parseUrl.query.iso;
+
+    let result;
+
+    if (!iso) {
+        res.writeHead(400, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify({ error: 'Missing iso query parameter' }));
+        return;
+    }
+
+    if (path === '/api/parsetime') {
+        result = parseTime(iso);
+    } else if (path === '/api/unixtime') {
+        result = unixTime(iso);
+    } 
+
+    if (result) {
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify(result));
+    } else {
+        res.writeHead(404, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify({ error: 'Not found' }));
+        return;
+    }
+});
+
+function parseTime(iso) {
+    const date = new Date(iso);
+
+    return {
+        hour: date.getHours(),
+        minute: date.getMinutes(),
+        second: date.getSeconds()
+    };
+}
+
+function unixTime(iso) {
+    return {
+        unixtime: new Date(iso).getTime()
+    };
+}
+
+server.listen(8000);
