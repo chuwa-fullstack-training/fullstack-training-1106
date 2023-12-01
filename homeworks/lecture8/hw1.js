5e8a2f0dbf46dbf0d758747ad66e64436fc4d205
smudgebaby <74436502+smudgebaby@users.noreply.github.com>
2023-12-01 16:28:45 -0500

    upload lecture8 hw1 and test folder

--- homeworks/lecture8/hw1.js
+++ homeworks/lecture8/hw1.js
@@ -9,3 +9,102 @@
  *    You don't need to handle the case like http://localhost:3000/hw1/test/test/txt.
  * 3. hw2 should be able to handle requests with query strings like it did in lecture 7;
  */
+
+// hw1
+const fs = require('fs');
+const path = require('path');
+
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+const router1 = express.Router();
+
+router1.get('/:dir/:ext', (req, res) => {
+    const directory = path.join(__dirname, req.params.dir);
+    const extension = req.params.ext;
+
+    fs.readdir(directory, (err, files) => {
+        if(err) {
+            console.log(err);
+            res.status(500).send('Internal Server Error');
+            return;
+        }
+        
+        const filteredFiles = files.filter(file => path.extname(file) === `.${extension}`)
+    
+        if(filteredFiles.length === 0){
+            res.send('No file with this extension!');
+            return;
+        }
+    
+        res.send(filteredFiles.join('\n'));
+    });
+})
+
+app.use('/hw1', router1);
+
+app.listen(PORT, () => {
+    console.log(`Express.js server is running on port ${PORT}`)
+})
+
+/**
+ * test: http://localhost:3000/hw1/testFolder/js
+ * output: test.js test2.js
+ * 
+ * test: http://localhost:3000/hw1/testFolder/txt
+ * output: No file with this extension!
+ * 
+ */
+
+
+
+
+// hw2
+const url = require('url');
+function parsetime(time) {
+    return {
+        hour: time.getHours(),
+        minute: time.getMinutes(),
+        second: time.getSeconds()
+    }
+}
+
+function unixtime(time) {
+    return {
+        unixtime: time.getTime()
+    }
+}
+
+const router2 = express.Router();
+router2.get('/parsetime', (req, res) => {
+    const parsedUrl = url.parse(req.url, true);
+    const time = new Date(parsedUrl.query.iso);
+    const result = parsetime(time);
+    res.send(result);
+})
+
+router2.get('/unixtime', (req, res) => {
+    const parsedUrl = url.parse(req.url, true);
+    const time = new Date(parsedUrl.query.iso);
+    const result = unixtime(time);
+    res.send(result);
+})
+
+app.use('/api', router2);
+
+
+/**
+ * test: http://localhost:3000/api/parsetime?iso=2023-05-22T12:34:56.789Z
+ * output:
+ * {
+ *  "hour": 12,
+ *  "minute": 34,
+ *  "second": 56
+ * }
+ * 
+ * test2: http://localhost:3000/api/unixtime?iso=2023-05-22T12:34:56.789Z
+ * output:
+ * { "unixtime": 1684758896789 }
+ *
+ */
