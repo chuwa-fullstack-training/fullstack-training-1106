4071004125e62e5864b5f25602e94a6856ee1522
Weslie2023 <swen3@stevens.edu>
2023-12-02 17:46:14 -0500

    hw_lecture7

--- homeworks/lecture7/hw1.js
+++ homeworks/lecture7/hw1.js
@@ -14,3 +14,21 @@
  */
 
 // your code here
+const fs = require('fs');
+const path = require('path');
+
+const directoryPath = process.argv[2];
+const fileExtension = `.${process.argv[3]}`;
+fs.readdir(directoryPath, (err, files) => {
+    if(err){
+        console.log(err)
+        console.log('Error when opening the directory.');
+    }else{
+        files.forEach(file => {
+            if(path.extname(file) === fileExtension){
+                console.log(file);
+            }
+        });
+
+    }
+})
--- homeworks/lecture7/hw3.js
+++ homeworks/lecture7/hw3.js
@@ -1,7 +1,7 @@
 /**
  * For sample code web-server.js, make the following changes:
  * Once submitting in home.html, stay on the same page and display the submitted data.
- * 
+ *
  * Hint:
  * 1. put the data of the submitted form in the query string of the url
  * 2. before res.end() in POST method, redirect to the home.html page with the query string
@@ -9,3 +9,65 @@
  * 3. you need to figure out how to parse the query string in the home.html page
  * 4. after writing the html content, you need to write the query string in the html as well
  */
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const querystring = require('querystring');
+
+const server = http.createServer((req, res) => {
+    const {url, method} = req;
+    if (method === 'GET') {
+        if (url === '/') {
+            res.end('this is the home page');
+        } else if (url === '/about') {
+            res.end('this is the about page');
+        } else if (url.startsWith('/home.html')) {
+            const query = url.split('?')[1];
+            const queryParams = querystring.parse(query);
+            let show = false;
+            if(query){
+                show = true;
+            }
+            fs.readFile(path.join(__dirname, 'home.html'), (err, html) => {
+                if (err) {
+                    res.end('error');
+                } else {
+                    res.writeHead(200, {'Content-Type': 'text/html'});
+                    res.write(html);
+
+                    if(show){
+                        res.write(`<p>Submitted Data: </p>`);
+                        for(const key in queryParams){
+                            res.write(`<pre>${key} : ${queryParams[key]}</pre>`);
+                        }
+                    }
+                    res.end();
+                }
+            });
+        } else {
+            res.end('this is the 404 page');
+        }
+    } else if (method === 'POST') {
+        if (url === '/create-post') {
+            let body = [];
+            req.on('data', chunk => {
+                body.push(chunk);
+            });
+            req.on('end', () => {
+                const parsedBody = Buffer.concat(body).toString();
+                res.statusCode = 302;
+                res.setHeader('Location', '/home.html?' + parsedBody);
+                res.end();
+
+            });
+        } else {
+            res.end('this is the 404 page');
+        }
+    } else {
+        res.end('Unsupported method');
+    }
+});
+
+server.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
