2f70d7d342244140cc1ded9c7ddb1de06a524c94
smudgebaby <74436502+smudgebaby@users.noreply.github.com>
2023-12-04 16:57:15 -0500

    upload lecture8 hw2 and 3 answers

--- homeworks/lecture8/hw2.js
+++ homeworks/lecture8/hw2.js
@@ -42,3 +42,50 @@
  *  }
  * }
  */
+
+
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/hw2', async (req, res) => {
+    try {
+      const { query1, query2 } = req.query;
+  
+      const result1 = await fetch(`https://hn.algolia.com/api/v1/search?query=${query1}&tags=story`);
+      const result2 = await fetch(`https://hn.algolia.com/api/v1/search?query=${query2}&tags=story`);
+  
+      const data1 = await result1.json();
+      const data2 = await result2.json();
+  
+      const combinedResult = {
+        [query1]: {
+          created_at: data1.hits[0].created_at,
+          title: data1.hits[0].title,
+        },
+        [query2]: {
+          created_at: data2.hits[0].created_at,
+          title: data2.hits[0].title,
+        },
+      };
+  
+      res.json(combinedResult);
+    } catch (error) {
+      res.status(500).json({ error: 'Error' });
+    }
+  });
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+
+/**
+ * http://localhost:3000/hw2?query1=apple&query2=banana
+ * {"apple":{"created_at":"2023-06-05T19:04:46Z","title":"Apple Vision Pro: Apple’s first spatial computer"},
+ * "banana":{"created_at":"2010-06-14T12:54:07Z","title":"Banana equivalent dose"}}
+ * 
+ * http://localhost:3000/hw2?query1=1234&query2=5678
+ * {"1234":{"created_at":"2017-05-04T18:04:01Z","title":"Edge displays “123456” in PDF but prints “114447”"},
+ * "5678":{"created_at":"2012-03-12T11:51:40Z","title":"Ask HN: Is there a standard for representing phone numbers in text?"}}
+ */
--- homeworks/lecture8/hw3.js
+++ homeworks/lecture8/hw3.js
@@ -1,3 +1,34 @@
 /**
  * Refactor hw3 in lecture 7 to use Express.js with template engine.
  */
+
+const path = require('path');
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => {
+    res.send('this is the home page');
+})
+app.get('/about', (req, res) => {
+    res.send('this is about page');
+})
+
+app.get('/home.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'home.html'));
+})
+
+app.post('/create-post', (req, res) => {
+    const formData = req.body;
+    res.redirect(`/home.html?${new URLSearchParams(formData).toString()}`)
+})
+
+app.use((req, res) => {
+    res.status(404).send('this is the 404 page')
+})
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
