fe6b85115cc99c0d860954839a62a66559059ccc
Guangwei Na <gna@stevens.edu>
2023-12-04 13:29:33 -0500

    samplecode modifications

--- sample_code/lecture8/middleware.js
+++ sample_code/lecture8/middleware.js
@@ -1,14 +1,15 @@
 const express = require('express');
+var path = require ('path');
 const app = express();
-const port = 3000;
+const port = 3002;
 
 const userController = require('./controller');
 const { checkAdmin } = require('./middleware/user');
-
+console.log(1111);
 // Set up view engine
 app.set('view engine', 'pug');
-app.set('views', './views');
-
+app.set('views', __dirname + '/views');
+console.log(path.join(__dirname + '/views'))
 // Set up middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -24,6 +25,7 @@ app.use((req, res, next) => {
 });
 
 app.get('/home.html', (req, res) => {
+  console.log(0);
   res.send('Home HTML');
 });
 
@@ -58,6 +60,8 @@ app.get(
 );
 
 app.get('/', (req, res) => {
+  console.log(1);
+  debugger
   res.render('mvc');
 });
 
@@ -65,4 +69,4 @@ app.get('*', (req, res) => {
   res.send('404 Not Found');
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => console.log(`Example1 app listening on port ${port}!`));
