b579d800ad636422ba143f11d9ae52a3c162e82a
PeachesHT <118096608+PeachesHT@users.noreply.github.com>
2023-12-07 01:13:02 -0500

    Add files via upload

--- homeworks/lecture10/routers/index.js
+++ homeworks/lecture10/routers/index.js
@@ -1,1 +1,42 @@
+const express = require('express');
+const router = express.Router();
+const {
+    getAllCompanies,
+    getAllEmployees,
+    getOneCompany,
+    getOneEmployee,
+    getEmployeeByCompany,
+    getEmployeesByManager,
+    createCompany,
+    createEmployee,
+    deleteCompany,
+    deleteEmployee,
+    updateCompany,
+    updateEmployee
+} = require('../controller/methods')
 
+router.get('/companies', getAllCompanies);
+
+router.get('/companies/:id', getOneCompany);
+
+router.get('/employees', getAllEmployees);
+
+router.get('/employees/:id', getOneEmployee);
+
+router.get('/companies/:id/employees', getEmployeeByCompany);
+
+router.get('/manager/:id/employees', getEmployeesByManager);
+
+router.post('/companies', createCompany);
+
+router.post('/employees', createEmployee);
+
+router.delete('/companies/:id', deleteCompany);
+
+router.delete('/employees/:id', deleteEmployee);
+
+router.put('/companies/:id', updateCompany);
+
+router.put('/employees/:id', updateEmployee);
+
+module.exports = router;
