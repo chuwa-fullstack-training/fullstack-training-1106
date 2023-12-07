d00c238de793dac3d388366693003f26535de396
PeachesHT <118096608+PeachesHT@users.noreply.github.com>
2023-12-07 01:13:47 -0500

    Add files via upload

--- homeworks/lecture10/database/connection.js
+++ homeworks/lecture10/database/connection.js
@@ -1,1 +1,15 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
 
+const connect = async () => {
+    await mongoose.connect(process.env.MONGODB_URI)
+    .then(() => {
+      console.log('Connected to MongoDB');
+    })
+    .catch(err => {
+      console.error(err.message);
+      console.log('Error connecting to MongoDB');
+      process.exit(1);
+    });
+}
+module.exports = connect;
