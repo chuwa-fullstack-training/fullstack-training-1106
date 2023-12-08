796d43ddb3bd6fec4a3f6de8559b2cb4e37da607
PeachesHT <118096608+PeachesHT@users.noreply.github.com>
2023-12-07 23:29:03 -0500

    Add files via upload

--- homeworks/lecture11/middlewares/auth.js
+++ homeworks/lecture11/middlewares/auth.js
@@ -1,1 +1,17 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config;
 
+module.exports = async (req, res, next) => {
+  const token =
+    req.header('x-auth-token') ||
+    req.headers?.authorization?.match(/^Bearer (.+)/)[1];
+  try {
+    const decoded = await jwt.verify(token, process.env.JWT_SECRET);
+    req.employee = decoded.employee;
+    req.isAuth = true;
+    next();
+  } catch (err) {
+    req.isAuth = false;
+    next();
+  }
+};
