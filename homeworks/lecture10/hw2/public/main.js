1211ae4caee6370aa5a1a54866beb2950ec2b8de
PeachesHT <118096608+PeachesHT@users.noreply.github.com>
2023-12-08 17:57:42 -0500

    Add files via upload

--- homeworks/lecture10/hw2/public/main.js
+++ homeworks/lecture10/hw2/public/main.js
@@ -1,1 +1,34 @@
+function handleCheck(ele) {
+    const id = ele.dataset.id;
+    fetch(`/todos/${id}`, {
+      method: 'PUT'
+    })
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+      });
+  }
 
+function handleSubmit() {
+    const description = document.querySelector('#todo').value;
+    if (!description) return alert('Please enter a todo');
+    fetch('/todos', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ description })
+    })
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+        window.location.reload();
+      });
+}
+  
+document.querySelector('#todo').addEventListener('keypress', function (e) {
+    if (e.key === 'Enter') {
+      handleSubmit();
+    }
+});
+  
