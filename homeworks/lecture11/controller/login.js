07a98e194a450ba5b14353fbacdac4af439503b5
PeachesHT <118096608+PeachesHT@users.noreply.github.com>
2023-12-07 23:20:37 -0500

    Add files via upload

--- homeworks/lecture11/controller/login.js
+++ homeworks/lecture11/controller/login.js
@@ -1,1 +1,33 @@
+const jwt = require('jsonwebtoken');
+const Employee = require('../models/Employee');
+require('dotenv').config;
 
+const login = async (req, res, next) => {
+    try {
+        const { username, password } = req.body;
+        const employee = await Employee.findOne({firstName: username});
+
+        if(!employee){
+            res.status(401).json({message: 'Invalid Credentials'});
+        }else if (employee.lastName !== password){
+            res.status(401).json({message: 'Invalid Credentials'});
+        }
+
+        const payload = {
+            employee: {
+                id: employee._id
+            }
+        };
+
+        const token = await jwt.sign(payload, process.env.JWT_SECRET, {
+            expiresIn: '1d'
+        });
+
+        res.json({token})
+    }catch (err) {
+        console.error(err.message);
+        res.status(500).json({message: 'Server Error'});
+    }
+}
+
+module.exports = login;
