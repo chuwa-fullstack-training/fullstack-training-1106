ff4faf62938069ba886fe679dc21aa8eaa6975b0
Boxiao Yan <boy033@ucsd.edu>
2023-12-07 23:11:55 -0800

    hw_9_10_11 setup

---
+++ homeworks/backend/mongo/connect.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+console.log(process.env.MONGODB_URI);
+
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch(err => {
+    console.log('Error connecting to MongoDB', err);
+  });
+
+module.exports = mongoose;
