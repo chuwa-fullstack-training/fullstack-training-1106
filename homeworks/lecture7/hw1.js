741b953e092e406ff6941abcc6273f36bd50ccd9
Damon <damon.liren@gmail.com>
2023-12-11 18:08:25 -0800

    l7

--- homeworks/lecture7/hw1.js
+++ homeworks/lecture7/hw1.js
@@ -14,3 +14,21 @@
  */
 
 // your code here
+const fs = require('fs');
+const path = require('path');
+
+const directory = process.argv[2];
+const extension = '.' + process.argv[3];
+
+fs.readdir(directory, (err, files) => {
+    if (err) {
+        console.error("Error reading directory:", err);
+        return;
+    }
+
+    files.forEach(file => {
+        if (path.extname(file) === extension) {
+            console.log(file);
+        }
+    });
+});
--- homeworks/lecture7/hw2.js
+++ homeworks/lecture7/hw2.js
@@ -19,3 +19,51 @@
  */
 
 // your code here
+const http = require('http');
+const url = require('url');
+
+const server = http.createServer((req, res) => {
+    const parsedUrl = url.parse(req.url, true);
+    const pathname = parsedUrl.pathname;
+    const isoTime = parsedUrl.query.iso;
+
+    if (!isoTime) {
+        res.writeHead(400);
+        res.end('Bad Request: iso parameter is missing');
+        return;
+    }
+
+    let date;
+    try {
+        date = new Date(isoTime);
+    } catch (error) {
+        res.writeHead(400);
+        res.end('Bad Request: invalid iso parameter');
+        return;
+    }
+
+    let result;
+    if (pathname === '/api/parsetime') {
+        result = {
+            hour: date.getHours(),
+            minute: date.getMinutes(),
+            second: date.getSeconds()
+        };
+    } else if (pathname === '/api/unixtime') {
+        result = {
+            unixtime: date.getTime()
+        };
+    } else {
+        res.writeHead(404);
+        res.end('Not Found');
+        return;
+    }
+
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify(result));
+});
+
+const PORT = 8081;
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
