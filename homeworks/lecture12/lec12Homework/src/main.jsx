f9c4e9f3c43397a1ad01f29a5ebc950296ae2477
smudgebaby <74436502+smudgebaby@users.noreply.github.com>
2023-12-13 00:18:10 -0500

    upload lecture12 hw1 answer

---
+++ homeworks/lecture12/lec12Homework/src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Hw1 from './Hw1.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Hw1 />
+  </React.StrictMode>,
+)
