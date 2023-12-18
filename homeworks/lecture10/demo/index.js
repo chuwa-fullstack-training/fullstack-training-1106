d12e6a54db364fcc98fef67e7ab626d662133f80
Kyrios0 <kyr1os@qq.com>
2023-12-18 05:14:53 -0800

    Lecture10 Homeworks

--- homeworks/lecture10/demo/index.js
+++ homeworks/lecture10/demo/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const app = express();
+const port = 3000;
 
 app.use(express.static('public'));
 app.use(express.json());
@@ -9,29 +12,52 @@ app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'pug');
 app.set('views', './views');
 
+mongoose.connect('mongodb://localhost:27017/hw10', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
+
+const todoSchema = new mongoose.Schema({
+  id: Number,
+  todo: String,
+  done: Boolean,
+});
+
+const Todo = mongoose.model('Todo', todoSchema);
+/*
 const todos = [
   { id: 1, todo: 'first thing', done: true },
   { id: 2, todo: 'second thing', done: false },
   { id: 3, todo: 'third thing', done: false }
 ];
-
-app.get('/', (req, res) => {
+*/
+app.get('/', async (req, res) => {
+  const todos = await Todo.find();
   res.render('index', { todos });
 });
 
-app.post('/api/todos', (req, res) => {
+app.post('/api/todos', async (req, res) => {
   const todo = req.body.todo;
-  todos.push({ id: todos.length + 1, todo, done: false });
+  const newTodo = new Todo({ id: todos.length + 1, todo, done: false });
+  await newTodo.save();
+  const todos = await Todo.find();
   res.json(todos);
 });
 
-app.put('/api/todos/:id', (req, res) => {
+app.put('/api/todos/:id', async (req, res) => {
   const id = parseInt(req.params.id, 10);
-  const todo = todos.find(t => t.id === id);
-  todo.done = !todo.done;
-  res.json(todo);
+  const existingTodo = await Todo.findOne({ id: todoId });
+  existingTodo.done = !existingTodo.done;
+  await existingTodo.save();
+  res.json(existingTodo);
 });
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log('Server is running on port 3000');
 });
--- homeworks/lecture10/hw1.md
+++ homeworks/lecture10/hw1.md
@@ -1,1 +1,21 @@
 Apply RESTful principles to the hw1 in lecture9
+
+see hw9 api.js
+/*
+    /api/company
+        -GET Get all companies
+        -POST Create a new company
+    /api/company/<:id>
+        -GET Get a company by id
+        -PUT Update a company by id
+        -DELETE Delete a company by id
+    /api/employee
+        -GET Get all employees
+        -POST Create a new employee
+    /api/employee/<:id>
+        -GET Get an employee by id
+        -PUT Update an employee by id
+        -DELETE Delete an employee by id
+    /api/company/<:id>/employees
+        -GET Get all employees of a company 
+*/
